2e990713a916ae0a05670c97d887ea40963a3097
Elias Khan <afkhan2@ualberta.ca>
2022-02-21 19:44:22 -0700

    create separate Auth class #18

--- app/src/main/java/com/cmput301w22t13/inquiry/MainActivity.java
+++ app/src/main/java/com/cmput301w22t13/inquiry/MainActivity.java
@@ -3,11 +3,9 @@ package com.cmput301w22t13.inquiry;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.cmput301w22t13.inquiry.auth.Auth;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,16 +13,12 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.cmput301w22t13.inquiry.databinding.ActivityMainBinding;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
-    private FirebaseAuth firebaseAuth;
-    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        // initialize Firebase Auth instance
-        firebaseAuth = FirebaseAuth.getInstance();
-        currentUser = firebaseAuth.getCurrentUser();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -47,23 +37,11 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        // check if user is signed in
-        if(currentUser == null) {
-            firebaseAuth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()) {
-                        Log.i("AUTH", "Successfully Authenticated");
-                    }
-                }
-            });
-        }
+        Auth.init();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        currentUser = firebaseAuth.getCurrentUser();
-    }
-
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//    }
 }
