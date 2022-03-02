b3b4e1d18c2f24f6684597f4d90d253cbe7e3c59
Ilia Kamyab <kamyabfa@ualberta.ca>
2022-03-02 15:52:28 -0700

    Initialize on create

--- app/src/main/java/com/cmput301w22t13/inquiry/geo/GeolocationSetup.java
+++ app/src/main/java/com/cmput301w22t13/inquiry/geo/GeolocationSetup.java
@@ -1,8 +1,35 @@
 package com.cmput301w22t13.inquiry.geo;
 
-import android.location.Location;
+import com.google.android.gms.location.LocationRequest;
+import android.os.Bundle;
 
-public class GeolocationSetup {
-    private Location location;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+public class GeolocationSetup extends AppCompatActivity {
+
+    public LocationRequest locationRequest;
+
+    private FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        locationRequest =  LocationRequest.create()
+                .setInterval(30000 )
+            .setFastestInterval(5000)
+            .setMaxWaitTime(100)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+
+    }
+
+    private void update(){
+        client = LocationServices.getFusedLocationProviderClient(GeolocationSetup.this);
+    }
 
 }
