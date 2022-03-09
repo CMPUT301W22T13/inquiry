0f6d2153164ed56d7dbbee706d7d399383d0b82d
Smehjabe <77705366+Smehjabe@users.noreply.github.com>
2022-03-09 16:12:23 -0700

    added edit profile activity

---
+++ app/src/main/java/com/cmput301w22t13/inquiry/activities/EditProfile.java
@@ -0,0 +1,16 @@
+package com.cmput301w22t13.inquiry.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cmput301w22t13.inquiry.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
