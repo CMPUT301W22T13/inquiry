8dc1741fb0fe4650f0d6d563412df9733860ea67
Elias <afkhan2@ualberta.ca>
2022-03-09 13:52:06 -0700

    create profile menu, fragment, and view

---
+++ app/src/main/java/com/cmput301w22t13/inquiry/ui/profile/ProfileViewModel.java
@@ -0,0 +1,19 @@
+package com.cmput301w22t13.inquiry.ui.profile;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ProfileViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is profile fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
