f3361fe1dcfb42c28e3a8c89d35b4e7004b43769
Elias <afkhan2@ualberta.ca>
2022-03-09 13:41:53 -0700

    generate and store random username for new users

--- app/src/main/java/com/cmput301w22t13/inquiry/auth/Auth.java
+++ app/src/main/java/com/cmput301w22t13/inquiry/auth/Auth.java
@@ -1,14 +1,18 @@
 package com.cmput301w22t13.inquiry.auth;
 
 import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.cmput301w22t13.inquiry.db.Database;
+import com.github.javafaker.Faker;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,28 +26,31 @@ public class Auth {
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
         Database db = new Database();
+        Faker faker = new Faker();
 
         // check if user is signed in
-        if(currentUser == null) {
+        if (currentUser == null) {
             firebaseAuth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()) {
+                    if (task.isSuccessful()) {
+                        String username = faker.superhero().prefix() + StringUtils.capitalize(faker.animal().name()) + faker.number().digits(2);
+
                         Map<String, Object> newUser = new HashMap<>();
                         String uid = Objects.requireNonNull(Objects.requireNonNull(task.getResult()).getUser()).getUid();
                         newUser.put("uid", uid);
+                        newUser.put("username", username);
                         db.put("users", newUser);
                     }
                 }
             });
-        }
-        else {
+        } else {
             Log.i("AUTH", "Current user: " + currentUser.getUid());
         }
     }
 
     public static FirebaseUser getCurrentUser() {
-        if(firebaseAuth != null) {
+        if (firebaseAuth != null) {
             return Auth.firebaseAuth.getCurrentUser();
         }
         return null;
