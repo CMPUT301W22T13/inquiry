c38e5683979621069a40859ef1c46b432f628a35
Smehjabe <77705366+Smehjabe@users.noreply.github.com>
2022-03-09 18:48:24 -0700

    edit profile fragment

--- app/src/main/java/com/cmput301w22t13/inquiry/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/cmput301w22t13/inquiry/ui/profile/ProfileFragment.java
@@ -1,19 +1,20 @@
 package com.cmput301w22t13.inquiry.ui.profile;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.cmput301w22t13.inquiry.R;
-import com.cmput301w22t13.inquiry.databinding.FragmentHomeBinding;
 import com.cmput301w22t13.inquiry.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
@@ -54,5 +55,23 @@ public class ProfileFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View root = binding.getRoot();
+        Button button = root.findViewById(R.id.edit_profile_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), EditProfile.class);
+                startActivity(intent);
+                ((Activity) getActivity()).overridePendingTransition(0, 0);
+            }
+        });
     }
 }
