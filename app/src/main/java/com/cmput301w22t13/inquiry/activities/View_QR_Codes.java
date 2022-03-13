8c9f2ef7c3502b3dae48f17f28bd52f258de65c1
kakaname <youngwo2@ualberta.ca>
2022-03-13 03:36:44 -0600

    pre swap

---
+++ app/src/main/java/com/cmput301w22t13/inquiry/activities/View_QR_Codes.java
@@ -0,0 +1,14 @@
+package com.cmput301w22t13.inquiry.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_QR_Codes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_qr_codes);
+    }
+}
