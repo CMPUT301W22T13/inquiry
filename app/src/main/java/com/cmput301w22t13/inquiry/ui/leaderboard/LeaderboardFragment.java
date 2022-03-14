1c5e66e1571f0da37fe5a9934f8928b8680d97ce
Elias <afkhan2@ualberta.ca>
2022-03-13 19:45:37 -0600

    fix leaderboard username query

--- app/src/main/java/com/cmput301w22t13/inquiry/ui/leaderboard/LeaderboardFragment.java
+++ app/src/main/java/com/cmput301w22t13/inquiry/ui/leaderboard/LeaderboardFragment.java
@@ -3,6 +3,7 @@ package com.cmput301w22t13.inquiry.ui.leaderboard;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.cmput301w22t13.inquiry.R;
@@ -23,49 +22,55 @@ import com.cmput301w22t13.inquiry.db.Database;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.local.QueryResult;
 
 import java.util.List;
 
 
 public class LeaderboardFragment extends Fragment {
 
-    private LeaderboardViewModel leaderboardViewModel;
     private FragmentLeaderboardBinding binding;
-    private Database db = new Database();
+    private final Database db = new Database();
 
     @SuppressLint("SetTextI18n")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        leaderboardViewModel =
-                new ViewModelProvider(this).get(LeaderboardViewModel.class);
+        LeaderboardViewModel leaderboardViewModel = new ViewModelProvider(this).get(LeaderboardViewModel.class);
 
         binding = FragmentLeaderboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textLeaderboard;
-        leaderboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        leaderboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         final EditText searchEditText = root.findViewById(R.id.leaderBoardEditTextSearch);
         final Button searchButton = root.findViewById(R.id.leaderBoardSearchButton);
         final TextView errorTextView = root.findViewById(R.id.leaderBoardSearchErrorTextView);
-        searchButton.setOnClickListener(view ->{
+        searchButton.setOnClickListener(view -> {
             String searchString = searchEditText.getText().toString();
-            Task<QuerySnapshot> queryTask= db.query("users", "username", searchString);
-            if(queryTask.isSuccessful()){
-                QuerySnapshot queryResults = queryTask.getResult();
-                List<DocumentSnapshot> documents = queryResults.getDocuments();
-                if (documents.size() != 0){
-                    DocumentSnapshot document = documents.get(0);
-                    String uid = (String) document.get("uid");
-                    Intent intent = new Intent(getActivity(), PlayerProfileActivity.class);
-                    intent.putExtra("uid", uid);
-                    startActivity(intent);
-                }else errorTextView.setText("no users found with username: " + searchString);
+            if (searchString.isEmpty()) {
+                searchEditText.setError("Please enter a username");
+            } else {
+                Task<QuerySnapshot> queryTask = db.query("users", "username", searchString.trim());
+                queryTask.addOnCompleteListener(task -> {
+                    if (queryTask.isSuccessful()) {
+                        QuerySnapshot queryResults = queryTask.getResult();
+                        List<DocumentSnapshot> documents = queryResults.getDocuments();
+                        if (documents.size() != 0) {
+                            Log.i("LeaderboardFragment", "Found users");
+                            Log.i("LeaderboardFragment", documents.toString());
+                            DocumentSnapshot document = documents.get(0);
+                            String uid = (String) document.get("uid");
+                            Intent intent = new Intent(getActivity(), PlayerProfileActivity.class);
+                            intent.putExtra("uid", uid);
+                            startActivity(intent);
+                        } else {
+                            Log.i("LeaderboardFragment", "No users found");
+                            errorTextView.setText("no users found with username: " + searchString);
+                        }
+                    } else {
+                        Log.i("LeaderboardFragment", "Query failed");
+                    }
+                });
+
             }
 
 
