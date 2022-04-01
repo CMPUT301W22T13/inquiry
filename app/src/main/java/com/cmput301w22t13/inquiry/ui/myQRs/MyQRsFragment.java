c901505cc5306e6fb5db9340be3c169c465f71ed
Ilia Kamyab <kamyabfa@ualberta.ca>
2022-04-01 14:52:34 -0600

    minor bug fixes

--- app/src/main/java/com/cmput301w22t13/inquiry/ui/myQRs/MyQRsFragment.java
+++ app/src/main/java/com/cmput301w22t13/inquiry/ui/myQRs/MyQRsFragment.java
@@ -1,8 +1,10 @@
 package com.cmput301w22t13.inquiry.ui.myQRs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,6 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cmput301w22t13.inquiry.R;
+import com.cmput301w22t13.inquiry.activities.MatchingQRActivity;
 import com.cmput301w22t13.inquiry.classes.QRCode;
 import com.cmput301w22t13.inquiry.databinding.FragmentMyqrsBinding;
 import com.cmput301w22t13.inquiry.db.onQrDataListener;
@@ -42,6 +45,7 @@ public class MyQRsFragment extends Fragment {
                 if (qrCodes != null && qrCodes.size() > 0) {
                     qrCodeListAdapter = new MyQRsListAdapter(root.getContext(), qrCodes);
                     recyclerView.setAdapter(qrCodeListAdapter);
+                    recyclerView.setClickable(true);
 
                     recyclerView.setVisibility(View.VISIBLE);
                     emptyStateText.setVisibility(View.GONE);
--- app/src/main/java/com/cmput301w22t13/inquiry/ui/myQRs/MyQRsListAdapter.java
+++ app/src/main/java/com/cmput301w22t13/inquiry/ui/myQRs/MyQRsListAdapter.java
@@ -1,15 +1,19 @@
 package com.cmput301w22t13.inquiry.ui.myQRs;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.cmput301w22t13.inquiry.R;
+import com.cmput301w22t13.inquiry.activities.MatchingQRActivity;
 import com.cmput301w22t13.inquiry.classes.QRCode;
 
 import java.util.ArrayList;
@@ -19,7 +23,7 @@ public class MyQRsListAdapter extends RecyclerView.Adapter<MyQRsListAdapter.View
     private final ArrayList<QRCode> qrCodes;
     private final Context context;
 
-    public MyQRsListAdapter(Context context, ArrayList<QRCode> qrCodes ) {
+    public MyQRsListAdapter(Context context, ArrayList<QRCode> qrCodes) {
         this.qrCodes = qrCodes;
         this.context = context;
     }
@@ -28,6 +32,14 @@ public class MyQRsListAdapter extends RecyclerView.Adapter<MyQRsListAdapter.View
     @Override
     public MyQRsListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = View.inflate(context, R.layout.myqrs_list_item, null);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "This is my Toast message!",
+                        Toast.LENGTH_LONG).show();
+                Log.d("HELLO","HELOOOOOOOOOOOOOO");
+            }
+        });
         return new ViewHolder(view);
     }
 
@@ -40,6 +52,22 @@ public class MyQRsListAdapter extends RecyclerView.Adapter<MyQRsListAdapter.View
         holder.nameTextView.setText(name);
         holder.initialsTextView.setText(initials);
         holder.scoreTextView.setText(String.valueOf(score) + " pts");
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Context context = view.getContext();
+                /*
+                Intent intent = new Intent(context, MatchingQRActivity.class);
+                intent.putExtra("code", qrCodes.get(holder.getAdapterPosition()));
+                intent.putExtra("player","YaBoyIlia");
+
+                 */
+                Toast.makeText(context, "This is my Toast message!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -47,7 +75,7 @@ public class MyQRsListAdapter extends RecyclerView.Adapter<MyQRsListAdapter.View
         return qrCodes.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener {
         TextView nameTextView;
         TextView initialsTextView;
         TextView scoreTextView;
@@ -57,6 +85,13 @@ public class MyQRsListAdapter extends RecyclerView.Adapter<MyQRsListAdapter.View
             nameTextView = itemView.findViewById(R.id.myqrs_qr_name);
             initialsTextView = itemView.findViewById(R.id.myqrs_qr_initials);
             scoreTextView = itemView.findViewById(R.id.myqrs_qr_score);
+
+        }
+
+
+        @Override
+        public void onClick(View view) {
+            Log.d("BITCH", "HOE");
         }
 
     }
