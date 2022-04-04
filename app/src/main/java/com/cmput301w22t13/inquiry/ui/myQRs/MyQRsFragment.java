da19c7323a8406eacef060d54d5069c96fecd86b
kakaname <youngwo2@ualberta.ca>
2022-04-04 09:11:56 -0600

    deleted unneeded code

--- app/src/main/java/com/cmput301w22t13/inquiry/ui/myQRs/MyQRsFragment.java
+++ app/src/main/java/com/cmput301w22t13/inquiry/ui/myQRs/MyQRsFragment.java
@@ -47,7 +47,7 @@ public class MyQRsFragment extends Fragment {
 
 
             Log.d("VERBS","is a Owner");
-            AllQRsViewModel allQRsViewModel = new ViewModelProvider(this).get(AllQRsViewModel.class);
+            MyQRsViewModel myQRsViewModel = new ViewModelProvider(this).get(MyQRsViewModel.class);
 
             binding = FragmentMyqrsBinding.inflate(inflater, container, false);
             View root = binding.getRoot();
@@ -59,7 +59,7 @@ public class MyQRsFragment extends Fragment {
 
             recyclerView.setLayoutManager(layoutManager);
 
-            allQRsViewModel.getOwner().fetchQRCodes(new onQrDataListener() {
+            myQRsViewModel.getPlayer().fetchQRCodes(new onQrDataListener() {
                 @Override
                 public void getQrData(ArrayList<QRCode> qrCodes) {
                     if (qrCodes != null && qrCodes.size() > 0) {
@@ -103,9 +103,9 @@ public class MyQRsFragment extends Fragment {
                         public void onClick(DialogInterface dialogInterface, int p) {
 
                             Log.d("VERBS",qrCode.getId());
-                            allQRsViewModel.deleteQRCode(qrCode);
+                            myQRsViewModel.deleteQRCode(qrCode);
                             Toast.makeText(getActivity(), "QR Code Deleted", Toast.LENGTH_SHORT).show();
-                            allQRsViewModel.getOwner().fetchQRCodes(task -> {
+                            myQRsViewModel.getPlayer().fetchQRCodes(task -> {
                                 qrCodeListAdapter.removeQrAt(position);
                                 if(task.size() == 0) {
                                     recyclerView.setVisibility(View.GONE);
